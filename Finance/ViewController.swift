98b0c39ff4a3261b958d2ff8099e5080e962845f
daryalitvinova <dlshazel30@gmail.com>
2025-07-20 00:24:14 +0700

    Initial Commit

---
+++ Finance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance
+//
+//  Created by Дарья Литвинова on 20.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FinanceTests/FinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTests.swift
+//  FinanceTests
+//
+//  Created by Дарья Литвинова on 20.07.2025.
+//
+
+import Testing
+@testable import Finance
+
+struct FinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
